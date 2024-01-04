39d060388072f75f239befab2efc52d7fbdb9910
raskovnik <4lch3my.3ck0@gmail.com>
2024-01-04 10:10:06 +0300

    ui enhancement

---
+++ flutter_movies_app/lib/presentation/list/movie_preview.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movies_app/domain/model/movie.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:intl/intl.dart';
+
+class MoviePreview extends StatelessWidget {
+  static const _size = 100.0;
+  final Movie movie;
+
+  const MoviePreview({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final DateFormat formatter = DateFormat.yMMMd();
+    
+    return Card(
+      child: Row(
+        children: [
+          if (movie.imageUrl != null)
+            Container(
+              padding: const EdgeInsets.only(
+                left: 8.0,
+                top: 8.0,
+                right: 16.0,
+                bottom: 8.0,
+              ), child: SizedBox(
+                width: _size,
+                height: _size,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5.0),
+                  child: CachedNetworkImage(
+                    imageUrl: movie.imageUrl!,
+                  ),
+                )
+              )
+            )
+          else 
+            Container(
+              padding: const EdgeInsets.only(
+                left: 8.0,
+                top: 8.0,
+                right: 16.0,
+                bottom: 8.0,
+              ),
+              child: _PlaceholderImage(size: _size)
+            ),
+            Flexible(
+              child: Container(
+                padding: const EdgeInsets.only(
+                left: 8.0,
+                top: 8.0,
+                right: 16.0,
+                bottom: 8.0,
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      movie.title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                      overflow: TextOverflow.ellipsis
+                    ),
+                    Padding(padding: const EdgeInsets.only(top: 12.0),
+                    child: Text(
+                      formatter.format(movie.releaseDate),
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),)
+                  ]
+                )
+              )
+              )
+            )
+        ]
+      )
+    );
+  }
+}
+
+class _PlaceholderImage extends StatelessWidget {
+  final double size;
+  const _PlaceholderImage({required this.size});
+
+  @override
+  Widget build(BuildContext context) => Icon(Icons.image_not_supported, size: size, color: Colors.black38);
+}
--- flutter_movies_app/lib/presentation/list/movies_list_screen.dart
+++ flutter_movies_app/lib/presentation/list/movies_list_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movies_app/data/repository/movies_repository.dart';
 import 'package:flutter_movies_app/domain/model/movie.dart';
+import 'package:flutter_movies_app/presentation/list/movie_preview.dart';
+import 'package:flutter_movies_app/presentation/list/movies_list_model.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 
 class MoviesListScreen extends StatefulWidget {
@@ -11,27 +15,50 @@ class MoviesListScreen extends StatefulWidget {
 }
 
 class _MoviesListScreenState extends State<MoviesListScreen> {
+  late final MoviesListModel _model;
+  final PagingController<int, Movie> _pagingController =
+      PagingController(firstPageKey: 1);
+
+  @override
+  void initState() {
+    super.initState();
+
+    _model = MoviesListModel(
+        log: Provider.of<Logger>(context, listen: false),
+        moviesRepo: Provider.of<MoviesRepository>(context, listen: false));
+
+    _pagingController.addPageRequestListener((pageKey) async {
+      try {
+        final movies = await _model.fetchPage(pageKey);
+        _pagingController.appendPage(movies, pageKey + 1);
+      } catch (e) {
+        _pagingController.error = e;
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    final moviesRepo = Provider.of<MoviesRepository>(context, listen: false);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Upcoming Movies")
-      ), 
-      body: FutureBuilder<List<Movie>>(
-        future: moviesRepo.getUpcomingMovies(limit: 10, page: 1),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView(
-              children: List.generate(snapshot.data!.length, (index) => ListTile(title: Text(snapshot.data![index].title))),);
-          } else if (snapshot.hasError) {
-      print("Error: ${snapshot.error}");
-      return const Text("Error loading data");
-    } else {
-            return const LinearProgressIndicator();
-          }
-        }
-      )
-    );
+        appBar: AppBar(title: const Text("Upcoming Movies")),
+        body: PagedListView<int, Movie>(
+          pagingController: _pagingController,
+          builderDelegate: PagedChildBuilderDelegate<Movie>(
+            itemBuilder: (context, movie, index) => Container(
+              padding: const EdgeInsets.only(
+                      left: 8.0,
+                      top: 8.0,
+                      right: 16.0,
+                      bottom: 8.0,
+                    ),
+                    child: MoviePreview(movie: movie),
+            )
+          )
+        ));
   }
 }
